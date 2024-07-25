6307e31000e5be0e8115a0aecb713d18835544d1
Galtbatzana <galt.batzana1@gmail.com>
2024-07-25 15:45:53 +0800

    start

---
+++ src/pages/components/hero.js
@@ -0,0 +1,10 @@
+export function Hero() {
+    return (
+        <div className="container">
+            <div className="grid grid-cols-3 gap-4">
+        
+            </div>
+        </div>
+    );
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "@/pages/components/Header"
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dev.to/api/articles?username=ben")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data);
+      });
+  }, []);
+
+  console.log({ articles });
 
   return (
-    <main className="mx-auto"> 
-      <Header/>
+    <main>
+      <div className="container">
+            <div className="grid grid-cols-3 gap-4">
+                {articles.map((item) => (
+                  <div key={item.url} target="_blank">
+                    {item.title}
+                  </div>
+                ))}
+        
+            </div>
+        </div>
 
     </main>
   );
