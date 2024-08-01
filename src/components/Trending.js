b99aa5e63bbd421d2fdecf4581adfc7a3b7346d8
Galtbatzana <galt.batzana1@gmail.com>
2024-08-01 18:16:20 +0800

    tredding

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -4,39 +4,51 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function Trending() {
-    
-    const [articles, setArticles] = useState([]);
-    
-    useEffect(() => {
-        fetch("https://dev.to/api/articles?username=ben")
-            .then((response)=> {
-            return response.json();
-            })
-            .then((data)=> {
-            setArticles(data);
-        });
-    
-    },[]);
+  const [articles, setArticles] = useState([]);
 
-    return (
-    <div className="container mx-auto">
-      <div className="carousel carousel-center rounded-box">
-        <div className="carousel-item">
-            {
-                articles.map((item) => (
-                    <div>
-                        <div key={item.id}>{item.title}</div>
-                        <Image  src={item.social_image} width={500} height={500}/>
-                    </div>        
-                ))
-            };
-            
-        </div>  
+  useEffect(() => {
+    fetch("https://dev.to/api/articles?username=ben")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data);
+      });
+  }, []);
+
+  return (
+    <div className="container mx-auto flex flex-col items-center">
+      <div className="carousel w-96 md:w-[750px] lg:w-[900px] gap-6 rounded-lg bg-black">
+        {articles.map((item) => (
+          <div key={item.id} className="carousel-item relative">
+            <Link href={item.path}>
+              <Image src={item.social_image} width={200} height={200} className="w-full size-60"/>
+            </Link>
+            <div className="absolute bottom-10 left-10 z-50">
+              <p className="badge badge-primary">{item.tag_list[0]}</p>
+              <div className="text-white rounded-lg ">{item.description}</div>
+            </div>
+          </div>
+        ))};
       </div>
+
+      {/* <div className="carousel rounded-box max-w-full">
+        {articles.map((item)=>(
+            <div className="carousel-item w-full relative">
+                <Link href={item.path}>
+                    <Image src={item.social_image} width={200} height={200} className="h-full size-48"/>
+                 </Link>
+                <div className="absolute bottom-10 left-10 z-50">
+                    <p className="badge badge-primary">{item.tag_list[0]}</p>
+                    <div className="text-white rounded-lg ">{item.description}</div>
+                </div>
+            </div>
+            
+        ))}
+        </div> */}
+
+
+
     </div>
   );
 }
-
-
-
-
